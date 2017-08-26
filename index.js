7c663b9f9018ea20d69138f5d2e0f2bba9b86a37
azoladyantyi <aizydyantyi@gmail.com>
2017-08-26 11:43:59 +0200

    i add new folders public and index file has been edited

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require('express');
+const exphbs = require("express-handlebars");
+const form = require('body-parser');
+var app = express();
 
-const app = express();
 
-app.get('/', function(req, res) {
-  res.send('greetings')
+// seting rendering engine
+app.engine("handlebars",exphbs({
+  defaultLayout:"main",
+  extname:"handlebars"
+}));
+app.use(express.static("public"));
+app.use(form.urlencoded({
+  extended:false
+}));
+app.set("view engine", "handlebars")
+
+app.get("/",function(req,res) {
+  res.render("home");
+});
+app.post("/",function(req,res){
+  var greet ='Hello ' + req.body.name
+  res.render('home', {
+    greetingMsg: greet
+  });
 });
 
-const port = 3000;
 
-app.listen(port, function () {
-  console.log('Web app started on port :' + port);
-});
+
+//start the server
+      var server = app.listen(3000, function() {
+          var host = server.address().address;
+          var port = server.address().port;
+
+          console.log('Example app listening at http://%s:%s', host, port);
+      });
