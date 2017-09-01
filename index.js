fbd87fd1b5b0a286e9b84a82339c305f5e671ab0
azoladyantyi <aizydyantyi@gmail.com>
2017-09-01 16:00:33 +0200

    updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.set("view engine", "handlebars")
 app.get("/", function(req, res) {
     res.render("home");
 });
-
+var list = [];
 var greeted = "";
 app.post("/", function(req, res) {
     var greet = req.body.name;
@@ -28,17 +28,24 @@ app.post("/", function(req, res) {
     } else if (language === "english") {
         greeted = "Hello, " + greet;
     } else if (language === "sotho") {
-        greeted = "Dumela, " + greet ;
+        greeted = "Dumela, " + greet;
     }
     res.render("home", {
         greetingMsg: greeted
     });
+     list.push(greet);
 });
 
+// console.log('listGreeted');
+app.get('/greeted', function(req, res) {
+    res.render('greeted', {
+      greetedPeeps : list
+    });
+});
 
 //start the server
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
